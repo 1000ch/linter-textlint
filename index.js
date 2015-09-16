c7137a9c98c005a82039e96e73e6f070563d4345
1000ch <shogo.sensui@gmail.com>
2015-09-16 17:49:08 +0900

    update

--- index.js
+++ index.js
@@ -1,39 +1,64 @@
 'use babel';
 
-import fs           from 'fs';
-import { Range }    from 'atom';
-import { textlint } from 'textlint';
-import helper       from 'atom-linter';
+import fs from 'fs';
+import path from 'path';
+import helper from 'atom-linter';
+import { Range } from 'atom';
+import { TextLintEngine } from 'textlint';
 
-export let config = {
-};
+export let config = {};
+
 const configFiles  = ['.textlintrc'];
 
 export const activate = () => {
-  console.log('linter-textlint');
   require("atom-package-deps").install("linter-textlint");
 };
 
 export const provideLinter = () => {
   return {
-    grammarScopes: ['source.md', 'source.markdown', 'source.txt'],
+    grammarScopes: ['source.gfm', 'source.pfm', 'source.txt'],
     scope: 'file',
     lintOnFly: true,
     lint: (editor) => {
 
-      console.log('linter-textlint.lint');
-
-      let path = editor.getPath();
+      let filePath = editor.getPath();
       let text = editor.getText();
-      let config = {};
+      let textlintConfig = {};
 
-      let configFile = helper.findFile(path, configFiles);
+      let configFile = helper.findFile(filePath, configFiles);
+
       if (configFile) {
-        config = JSON.parse(fs.readFileSync(configFile));
+        textlintConfig = JSON.parse(fs.readFileSync(configFile));
       }
 
-      let results = textlint.lintText(text);
-      console.log(results);
+      const projectDirectory = atom.project.getPaths().shift();
+      const nodeModules = path.join(projectDirectory, './node_modules/textlint-rule-max-ten');
+
+      if (!Array.isArray(textlintConfig.rulePaths)) {
+        textlintConfig.rulePaths = [nodeModules];
+      } else {
+        textlintConfig.rulePaths.push(nodeModules);
+      }
+
+      const textlint = new TextLintEngine(textlintConfig);
+      const items = textlint.executeOnText(text)
+        .filter(result => result.messages.length)
+        .map(result => result.messages.shift());
+
+      return items.map(item => {
+
+        let range = new Range(
+          [item.loc.start.line - 1, item.loc.start.column],
+          [item.loc.end.line - 1, item.loc.end.column]
+        );
+
+        return {
+          type: item.type,
+          text: item.message,
+          filePath: filePath,
+          range: range
+        };
+      });
     }
   };
 };
