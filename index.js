9cea12ddc1036f9886a065a759e96e0e295bbd68
azu <azuciao@gmail.com>
2015-11-18 23:11:18 +0900

    update textlint to 5.0.0
    use async API

--- index.js
+++ index.js
@@ -16,49 +16,51 @@ export const provideLinter = () => {
     scope: 'file',
     lintOnFly: true,
     lint: (editor) => {
+      return new Promise((resolve, reject) => {
+        let directory = atom.project.rootDirectories[0];
 
-      let directory = atom.project.rootDirectories[0];
+        if (!directory) {
+          return resolve([]);
+        }
 
-      if (!directory) {
-        return [];
-      }
+        let configFile = directory.resolve('./.textlintrc');
+        let pluginPath = directory.resolve('./node_modules/');
 
-      let configFile = directory.resolve('./.textlintrc');
-      let pluginPath = directory.resolve('./node_modules/');
+        // do not lint if .textlintrc does not exist
+        if (!fs.existsSync(configFile) || !fs.existsSync(pluginPath)) {
+          return resolve([]);
+        }
 
-      // do not lint if .textlintrc does not exist
-      if (!fs.existsSync(configFile) ||
-          !fs.existsSync(pluginPath)) {
-        return [];
-      }
+        let textlint = new TextLintEngine({
+          configFile: configFile,
+          rulesBaseDirectory: pluginPath
+        });
 
-      let textlint = new TextLintEngine({
-        configFile: configFile,
-        rulesBaseDirectory: pluginPath
-      });
+        let filePath = editor.getPath();
+        textlint.executeOnFiles([filePath]).then(results => {
+          const push = Array.prototype.push;
+          const messages = [];
+          results.filter(result => result.messages.length)
+              .forEach(result => push.apply(messages, result.messages));
 
-      let filePath = editor.getPath();
+          const lintMessages = messages.map(message => {
+            // line and column 1-based index
+            // https://github.com/azu/textlint/blob/master/docs/use-as-modules.md
+            let range = new Range(
+                [message.line - 1, message.column - 1],
+                [message.line - 1, message.column - 1]
+            );
 
-      const messages = [];
-      const push = Array.prototype.push;
-      textlint.executeOnFiles([filePath])
-        .filter(result => result.messages.length)
-        .forEach(result => push.apply(messages, result.messages));
+            return {
+              type: textlint.isErrorMessage(message) ? "Error" : "Warning",
+              text: message.message,
+              filePath: filePath,
+              range: range
+            };
+          });
 
-      return messages.map(message => {
-        // line and column 1-based index
-        // https://github.com/azu/textlint/blob/master/docs/use-as-modules.md
-        let range = new Range(
-          [message.line - 1, message.column - 1],
-          [message.line - 1, message.column - 1]
-        );
-
-        return {
-          type: textlint.isErrorMessage(message) ? "Error" : "Warning",
-          text: message.message,
-          filePath: filePath,
-          range: range
-        };
+          resolve(lintMessages);
+        });
       });
     }
   };
